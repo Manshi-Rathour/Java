27f2a3622b89b8fa6725cab8a8522c2e76550913
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-01-20 23:13:51 +0530

    java basics

---
+++ SumOfNumbers.java
@@ -0,0 +1,11 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        short num1 = 2;
+        short num2 = 3;
+        short num3 = 4;
+
+        int sum = num1 + num2 + num3;
+
+        System.out.println(sum);
+    }
+}
