ec85c2ef0b1ab7bdf96098797a0c3d9cce79bc8a
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-02-04 12:02:20 +0530

    oops basic

---
+++ OOPs_Custom_class.java
@@ -0,0 +1,37 @@
+public class OOPs_Custom_class {
+
+    static class Employee{
+        int id;
+        String name;
+        int salary;
+
+        public void printDetails(){
+            System.out.println("Id is : " + id);
+            System.out.println("Name of the employee is : " + name);
+        }
+
+        public int getSalary(){
+            return salary;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is a custom class.");
+
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.id = 12;
+        e1.name = "Manshi";
+        e1.salary = 200000;
+
+        e2.id = 15;
+        e2.name = "Ananya";
+        e2.salary = 100000;
+
+        e1.printDetails();
+        System.out.println("Salary is : " + e1.getSalary());
+
+        e2.printDetails();
+        System.out.println("Salary is : " + e2.getSalary());
+    }
+}
