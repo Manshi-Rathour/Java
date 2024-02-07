dfe4cc100ac070f70a775edcd8f0cbc910f1c851
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-02-07 20:44:07 +0530

    updated dynamic method dispatch

--- DynamicMethodDispatch.java
+++ DynamicMethodDispatch.java
@@ -19,6 +19,7 @@ class SmartPhone extends Phone{
 public class DynamicMethodDispatch {
     public static void main(String[] args) {
         Phone obj = new SmartPhone();
+        // SmartPhone obj2 = new Phone(); // Not allowed
         obj.on();
         obj.showTime();
         // obj.music(); // This will throw error because reference is Phone class, and we are accessing method of SmartPhone class
