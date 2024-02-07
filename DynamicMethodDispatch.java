64f07d72c2bbafad3105136b0fea7a25ac6cf4f4
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-02-07 20:42:39 +0530

    Dynamic method dispatch

---
+++ DynamicMethodDispatch.java
@@ -0,0 +1,26 @@
+class Phone{
+    public void showTime(){
+        System.out.println("Time is 8 pm");
+    }
+    public void on(){
+        System.out.println("Turning On the Phone...");
+    }
+}
+
+class SmartPhone extends Phone{
+    public void on(){
+        System.out.println("Turning On the SmartPhone...");
+    }
+    public void music(){
+        System.out.println("Playing the music...");
+    }
+}
+
+public class DynamicMethodDispatch {
+    public static void main(String[] args) {
+        Phone obj = new SmartPhone();
+        obj.on();
+        obj.showTime();
+        // obj.music(); // This will throw error because reference is Phone class, and we are accessing method of SmartPhone class
+    }
+}
