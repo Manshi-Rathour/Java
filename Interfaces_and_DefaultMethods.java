708bdd6892db8971c81c1f377b1941e1738c0a32
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-02-08 23:48:39 +0530

    interfaces and default methods

--- Interfaces_and_DefaultMethods.java
+++ Interfaces_and_DefaultMethods.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Interfaces_and_DefaultMethods {
+interface MyCamera{
+    void takeSnap();
+    void recordVideo();
+    private void greet(){
+        System.out.println("Good Morning");
+    }
+    default void record4KVideo(){
+        greet();
+        System.out.println("Recording in 4k...");
+    }
+}
+
+interface MyWifi{
+    String[] getNetworks();
+    void connectToNetwork(String network);
+}
+
+class MyCellPhone{
+    void callNumber(int phoneNumber){
+        System.out.println("Calling "+ phoneNumber);
+    }
+    void pickCall(){
+        System.out.println("Connecting... ");
+    }
+}
+
+class MySmartPhone extends MyCellPhone implements MyWifi, MyCamera{
+    public void takeSnap(){
+        System.out.println("Taking snap");
+    }
+    public void recordVideo(){
+        System.out.println("Taking snap");
+    }
+//    public void record4KVideo(){
+//       System.out.println("Taking snap and recoding in 4k");
+//    }
+    public String[] getNetworks(){
+        System.out.println("Getting List of Networks");
+        String[] networkList = {"Manshi", "Anjali", "MJ"};
+        return networkList;
+    }
+    public void connectToNetwork(String network){
+        System.out.println("Connecting to " + network);
+    }
+}
+
+public class Interfaces_and_DefaultMethods {
+    public static void main(String[] args) {
+        MySmartPhone ms = new MySmartPhone();
+        ms.record4KVideo();
+        // ms.greet(); --> Throws an error!
+        String[] ar = ms.getNetworks();
+        for (String item: ar) {
+            System.out.println(item);
+        }
+    }
 }
