48301a34852fd74f63682f42467ea8ef8cbf61bb
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-03-07 10:43:44 +0530

    finally block

---
+++ FinallyBlock.java
@@ -0,0 +1,40 @@
+public class FinallyBlock {
+
+    public static int greet(){
+        try{
+            int a = 10;
+            int b = 2;
+            int c = a/b;
+            return c;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Cleaning up resources....This is the end of this function");
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int k = greet();
+        System.out.println(k);
+
+        int a = 10;
+        int b = 9;
+        while(true){
+            try{
+                System.out.println("a/b : " + a/b);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                break;
+            }
+            finally {
+                System.out.println("I am finally for value of b = " + b);
+            }
+            b--;
+        }
+    }
+
+}
